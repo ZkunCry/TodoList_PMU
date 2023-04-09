ec7de1bb1f9617f4e89a5d8d375d4b3855b6759f
ZkunCry <aksentev.03@mail.ru>
2023-04-09 23:01:25 +0500

    project init

---
+++ TodolistMVVM/TodolistMVVM/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using TodolistMVVM;
+using System.Collections.ObjectModel;
+
+namespace TodolistMVVM
+{
+    public partial class MainPage : ContentPage
+    {
+ 
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new TaskListView.TaskListView();
+           
+        }
+    }
+}
---
+++ TodolistMVVM/TodolistMVVM/TaskViewModel/TaskViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using TodolistMVVM.TaskModel;
+
+namespace TodolistMVVM.TaskViewModel
+{
+    class TaskViewModel:INotifyPropertyChanged //Класс, который представляет 
+    {
+       public event PropertyChangedEventHandler PropertyChanged;
+
+       private TaskModel.TaskModel task;
+       public TaskViewModel()
+        {
+            task = new TaskModel.TaskModel { isFinished = false };
+        }
+        public string TextTask { get=>task.TextTask; 
+            set 
+            { 
+                task.TextTask = value;
+                OnPropertyChanged("TextTask");
+            } 
+        }
+
+        public bool isFinished { get => task.isFinished;
+            set
+            {
+                task.isFinished = task.isFinished != false ? true : false;
+                OnPropertyChanged("Finished");
+            }
+            
+        }
+        protected void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+    }
+}
